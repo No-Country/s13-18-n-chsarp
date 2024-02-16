c23cf7c3f122c84661f87ed3f4ff0d52ec75bcb0
GoRiJ <jolem824@gmail.com>
2024-02-15 20:36:02 -0600

    feat: modal store adition

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,2 +1,3 @@
 export * from './api.model';
 export * from './app.model';
+export * from './modal.model';
---
+++ src/models/modal.model.ts
@@ -0,0 +1,9 @@
+/**
+ * Modal types enum.
+ */
+export enum ModalType {
+  CREATE_CHANNEL = 'createChannel',
+  DELETE_CHANNEL = 'deleteChannel',
+  EDIT_CHANNEL = 'editChannel',
+  INVITE = 'invite',
+}
