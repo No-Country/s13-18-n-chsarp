c4ec2b084c6a061c47efd503e4178dfef027a45e
scarletvargas <scarletvf47@gmail.com>
2024-02-23 21:51:26 -0400

    Arreglando el bug en el header

--- src/app/(landing)/components/header.tsx
+++ src/app/(landing)/components/header.tsx
@@ -4,7 +4,7 @@ import { Nav } from './nav';
 
 export const Header: FC = (): ReactElement => {
   return (
-    <header className="fixed top-0 bg-background w-full">
+    <header className="fixed top-0 bg-white w-full z-10 shadow-4xl">
       <Nav />
     </header>
   );
