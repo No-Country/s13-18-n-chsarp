0f9152317f801d100daa11d128fc71873e54a4a9
jp-quintana <juanquintana1996@gmail.com>
2024-02-27 20:49:47 -0300

    feat: add chat header

--- src/components/chat-header.tsx
+++ src/components/chat-header.tsx
@@ -1,21 +1,35 @@
 'use client';
 
+import { cn } from '@/lib';
 import { CircleEllipsis, Phone, Video, XCircle } from 'lucide-react';
+import { useTheme } from 'next-themes';
 import { Button } from './ui';
 
 export const ChatHeader = () => {
+  const { resolvedTheme } = useTheme();
+
   return (
-    <div className="flex justify-between items-center text-white border-b-2 border-white pb-6 gap-12 ">
-      <CircleEllipsis />
+    <div className="flex justify-between items-center text-white border-b-2 border-white dark:border-black pb-6 gap-12">
+      <CircleEllipsis className="dark:text-black" />
       <div className="flex flex-1 justify-end">
-        <Button className="rounded-r-none dark">
-          <Phone className="dark" />
+        <Button
+          className={cn(
+            'rounded-r-none',
+            resolvedTheme === 'dark' ? 'not-dark' : 'dark'
+          )}
+        >
+          <Phone />
         </Button>
-        <Button className="rounded-l-none dark">
-          <Video className="dark" />
+        <Button
+          className={cn(
+            'rounded-l-none',
+            resolvedTheme === 'dark' ? 'not-dark' : 'dark'
+          )}
+        >
+          <Video />
         </Button>
       </div>
-      <XCircle />
+      <XCircle className="dark:text-black" />
     </div>
   );
 };
