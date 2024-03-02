df46ccf5845283fb86cbb4013ac8b3620686c156
Emanuel-Capo <emanuel_c400@hotmail.com>
2024-03-02 19:49:01 -0300

    Se agregó JWT al chat

--- server/api/Domain/HubModels/UserConnection.cs
+++ server/api/Domain/HubModels/UserConnection.cs
@@ -3,7 +3,7 @@
     public class UserConnection
     {
         public int SessionId { get; set; }
-        public string UserName { get; set; } = string.Empty;
+        //public string UserName { get; set; } = string.Empty;
         public string ChatRoom { get; set; } = string.Empty;
     }
 }
