fabec5235e83d590955b0e984ad32ae9b5037cb6
scarletvargas <scarletvf47@gmail.com>
2024-03-06 20:08:41 -0400

    agregando nuevo component y actualizando

--- src/app/(main)/components/index.ts
+++ src/app/(main)/components/index.ts
@@ -1,5 +1,5 @@
 export * from './add-channel';
 export * from './avatar-icon';
 export * from './dropdown';
-export * from './info-chat.dialog';
 export * from './sidebar';
+export * from './modal-info';
